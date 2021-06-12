dcf4f9739159e0f0019692263c2f6d87f4c39142
VialOVice <vialovice@gmail.com>
2021-06-13 01:12:29 +0200

    yes
    yes

--- Assets/Scripts/SelectionController.cs
+++ Assets/Scripts/SelectionController.cs
@@ -9,17 +9,19 @@ public class SelectionController : MonoBehaviour
     private readonly int buildLayerMask = 1 << 7;
     private readonly int resourceLayerMask = 1 << 8;
     private int ObjectLayerMask;
+    private InputManager inputManager;
     // Start is called before the first frame update
     void Start()
     {
         ObjectLayerMask = buildLayerMask | resourceLayerMask;
+        inputManager = InputManager.Instance;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetMouseButtonDown(0))
+        if (inputManager.)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
