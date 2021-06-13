0e9ac311fdb6f9bd8292bd801a70fdb211e9295f
VialOVice <vialovice@gmail.com>
2021-06-13 16:44:59 +0200

    fogofwar color and a bit in the selection

--- Assets/Scripts/SelectionController.cs
+++ Assets/Scripts/SelectionController.cs
@@ -6,8 +6,8 @@ public class SelectionController : MonoBehaviour
 {
 
     RaycastHit hit;
-    private readonly int buildLayerMask = 1 << 7;
-    private readonly int resourceLayerMask = 1 << 8;
+    private readonly int buildLayerMask = LayerMask.NameToLayer("Buildings");
+    private readonly int resourceLayerMask = LayerMask.NameToLayer("Resources");
     private int ObjectLayerMask;
 
 
@@ -24,10 +24,8 @@ public class SelectionController : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(mousePos);
-
-            Debug.Log("TODO");
-
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ObjectLayerMask))
+            
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, buildLayerMask))
             {
                 Debug.Log("TODO");
                 //todo call the interface function to open up infos about the object
